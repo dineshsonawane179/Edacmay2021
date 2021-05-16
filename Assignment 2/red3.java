9ef25e28a67b229f5d25ba6fda308073b909094a
dineshsonawane179 <83825783+dineshsonawane179@users.noreply.github.com>
2021-05-16 22:42:47 +0530

    Add files via upload

---
+++ Assignment 2/red3.java
@@ -0,0 +1,39 @@
+public class red3
+{ 
+
+public static void main(String[]args){
+
+for(int i=6;i>=1;i--)
+{    
+	if(i==6)
+		{
+	         for(int t=1;t<=6;t++)
+			{
+	
+				System.out.print("*");
+			}
+      
+		}
+	
+	else
+	{
+	
+			
+			System.out.print("*");
+			for(int k=1;k<=i-2;k++)
+			{
+	
+				System.out.print(" ");
+				
+			}
+			System.out.print("*");
+			
+		}
+		
+	 
+System.out.println();
+
+}
+System.out.print("*");
+}
+}
